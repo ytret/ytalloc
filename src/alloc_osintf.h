1f2d5308865c0ac2c975958d914fffb2a50367c7
Yuri Tretyakov <ytretyakov18@gmail.com>
2025-08-31 10:47:06 +0300

    Add CMake options to control memcpy and memset function names

--- src/alloc_osintf.h
+++ src/alloc_osintf.h
@@ -3,12 +3,20 @@
 #include <stddef.h>
 #include <string.h>
 
+#ifndef YTALLOC_MEMCPY
+#define YTALLOC_MEMCPY memcpy
+#endif
+
+#ifndef YTALLOC_MEMSET
+#define YTALLOC_MEMSET memset
+#endif
+
 [[gnu::always_inline]]
 inline void *alloc_memcpy(void *dest, const void *src, size_t n) {
-    return memcpy(dest, src, n);
+    return YTALLOC_MEMCPY(dest, src, n);
 }
 
 [[gnu::always_inline]]
 inline void *alloc_memset(void *s, int c, size_t n) {
-    return memset(s, c, n);
+    return YTALLOC_MEMSET(s, c, n);
 }
