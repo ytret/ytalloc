d6ad29a7fae0b08d5b101d51f634ad101fbc8443
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-01-23 14:45:26 +0300

    alloc_osintf.h: declare memcpy, memset and abort functions

--- src/alloc_osintf.h
+++ src/alloc_osintf.h
@@ -2,22 +2,9 @@
 
 #include <stddef.h>
 
-#ifdef YTALLOC_HAS_STDLIB
-#include <stdlib.h>
-#include <string.h>
-#endif
-
-#ifndef YTALLOC_MEMCPY
-#define YTALLOC_MEMCPY memcpy
-#endif
-
-#ifndef YTALLOC_MEMSET
-#define YTALLOC_MEMSET memset
-#endif
-
-#ifndef YTALLOC_ABORT
-#define YTALLOC_ABORT abort
-#endif
+void *YTALLOC_MEMCPY(void *dest, const void *src, size_t n);
+void *YTALLOC_MEMSET(void *s, int c, size_t n);
+void YTALLOC_ABORT(void);
 
 [[gnu::always_inline]]
 inline void *alloc_memcpy(void *dest, const void *src, size_t n) {
